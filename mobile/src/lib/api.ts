e6159166cedb2bc5845b9be62b490223b8b66a4b
Gonçalo Pinto <goncalopintoneto@gmail.com>
2023-05-19 12:40:34 +0100

    04 - Integrando os Projetos Web e Mobile

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.76:3333',
+  baseURL: 'http://172.20.10.2:3333', // HOME
+  // baseURL: 'http://89.114.69.149:3333', // PHONE
 })
